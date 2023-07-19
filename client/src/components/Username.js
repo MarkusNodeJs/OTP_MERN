567b55fb1e8d3b54f7f9a372fbeafc0cb3581732
MarkusNodeJs <markus.dalton03282023@gmail.com>
2023-07-19 16:14:28 +0800

    update

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
 
 import styles from "../styles/Username.module.css";
 
 export default function Username() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+    },
+    validateOnBLur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center h-screen">
@@ -17,7 +30,7 @@ export default function Username() {
             {/* -------- div end ----------- */}
           </div>
           {/*-------------------Form------------------*/}
-          <form className="py-1">
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img
                 src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
@@ -28,6 +41,7 @@ export default function Username() {
 
             <div className="textbox flex flex-col item-center gap-6">
               <input
+                {...formik.getFieldProps("username")}
                 className={styles.textbox}
                 type="text"
                 placeholder="Username"
